c417e28e70861bde545cd114f0ad4d4497749077
heliaea <lunasci@protonmail.com>
2023-07-11 11:04:05 +0200

    Code cleaned

--- SaliencyRanking/main.py
+++ SaliencyRanking/main.py
@@ -4,17 +4,17 @@ import matplotlib.pyplot as plt
 from saraRC1 import *
 
 def main():
-    im = "./beer_mug_colour.jpeg"
+    im = '../COTS Dataset/Part 1 - Single Objects/objects/buddha_colour.jpeg'
     s1 = cv2.imread(im)
 
-    # cv2.imshow("Input Image", s1)
+    # cv2.imshow('Input Image', s1)
 
     sara, sara_list = return_sara(s1)
-    cv2.imshow("SaRa Output", sara)
+    cv2.imshow('SaRa Output', sara)
     # print(sara_list)
 
     cv2.waitKey()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
